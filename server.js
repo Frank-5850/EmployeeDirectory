a4212c6702e75f85beef3dabde35f215cfd60aa7
Franco <phanfranco@yahoo.com>
2021-03-15 23:49:49 -0700

    Created route to hit backend.

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const apiRoutes = require("./routes/apiRoutes");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(apiRoutes);
 
 app.listen(PORT, () => {
   console.log(`listening at: http://localhost:${PORT}`);
